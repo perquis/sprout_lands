1266276e9ca5f8f984997a6f84c7f51873c72e30
PerQuis! <thetahirpl@gmail.com>
2023-09-30 13:29:41 +0200

    👀 refactor: Refactored game.py.

--- game.py
+++ game.py
@@ -2,27 +2,23 @@ from packages import resources, models
 import pygame
 
 pygame.init()
-pygame.display.toggle_fullscreen()
 
 clock = pygame.time.Clock()
 infoObject = pygame.display.Info()
 screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
-
-resources.music("village_vibe.mp3")
 device = models.Device()
 
-running = True
-dt = 0
+pygame.display.toggle_fullscreen()
+resources.music("village_vibe.mp3")
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+running = True
+dt = 0
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill("black")
-    pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
 
@@ -36,6 +32,10 @@ while running:
         player_pos.x += 300 * dt
     if keys[pygame.K_ESCAPE]:
         running = False
+
+    # Drawing
+    screen.fill("black")
+    pygame.draw.circle(screen, "red", player_pos, 40)
 
     pygame.display.update()
     dt = clock.tick(device.get_refresh_rate()) / 1000
