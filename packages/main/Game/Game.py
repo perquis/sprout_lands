170b6eb813d2e1981c410141eccf0768030aa8eb
PerQuis! <thetahirpl@gmail.com>
2023-10-13 23:03:23 +0200

    ✨feature: Introduced fixes to class Game.

--- packages/main/Game/Game.py
+++ packages/main/Game/Game.py
@@ -1,83 +1,51 @@
 import pygame
 
-from packages import models, resources
 from packages.enums import Direction, Gender
+from packages.main.Game.Settings import Settings
 from packages.models import Player
 
 
-class Game:
+class Game(Settings):
     def __init__(self) -> None:
-        pygame.init()
-        device = models.Device()
-        resolution = pygame.display.Info()
-
-        self.start = True
-        self.clock = pygame.time.Clock()
-        self.display = resolution.current_w, resolution.current_h
-        self.screen = pygame.display.set_mode(self.display, pygame.RESIZABLE)
-        self.refresh_rate = device.get_refresh_rate()
-        self.current_direction = Direction.DOWN
-        self.delta_time = 0.0
-        self.speed = 225
-
-        self.player_pos = pygame.Vector2(
-            self.screen.get_width() / 2, self.screen.get_height() / 2)
+        super().__init__()
 
     def run(self):
-        pygame.mouse.set_visible(False)
-        resources.music("village_vibe.mp3")
         self.toggle_fullscreen()
+        self.mouse_visible(False)
+        self.load_music("village_vibe.mp3")
 
         sprites = pygame.sprite.Group()  # type: ignore
-
         player = Player("PerQuis", Gender.MALE)
         sprites.add(player)
 
         while self.start:
-            for event in pygame.event.get():
-                # close the window by clicking the X button
-                if event.type == pygame.QUIT:
-                    self.start = False
-
-                if event.type == pygame.KEYUP and event.key == pygame.K_F11:
-                    self.toggle_fullscreen()
-
-            # get the pressed key
-            keys = pygame.key.get_pressed()
-
-            if keys[pygame.K_w] or keys[pygame.K_UP] or keys[pygame.K_s] or keys[pygame.K_DOWN] or keys[pygame.K_a] or keys[
-                    pygame.K_LEFT] or keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            if any(self.all_direction_keys):
                 player.toggle_move(True)
             else:
                 player.toggle_move(False)
 
-            # move the player
-            if keys[pygame.K_w] or keys[pygame.K_UP]:
+            if any(self.direction_up_keys):
                 self.current_direction = Direction.UP
                 self.player_pos.y -= self.speed * self.delta_time
-            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            if any(self.direction_down_keys):
                 self.current_direction = Direction.DOWN
                 self.player_pos.y += self.speed * self.delta_time
-            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            if any(self.direction_left_keys):
                 self.current_direction = Direction.LEFT
                 self.player_pos.x -= self.speed * self.delta_time
-            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            if any(self.direction_right_keys):
                 self.current_direction = Direction.RIGHT
                 self.player_pos.x += self.speed * self.delta_time
 
-            if keys[pygame.K_ESCAPE]:
-                self.start = False
+            if self.keys[pygame.K_ESCAPE]:
+                self.exit()
 
             self.screen.fill("black")
             sprites.draw(self.screen)
             # update the player position and animation speed
             sprites.update(self.player_pos, 0.25, self.current_direction)
 
-            pygame.display.update()
-            self.delta_time = self.clock.tick(self.refresh_rate) / 1000
+            self.update()
+            self.handle_events()
 
         pygame.quit()
-
-    def toggle_fullscreen(self):
-        """Toggle fullscreen mode."""
-        pygame.display.toggle_fullscreen()
---
+++ packages/main/Game/Settings.py
@@ -0,0 +1,87 @@
+from abc import ABC
+from typing import List
+
+import pygame
+
+from packages import models, resources
+from packages.enums import Direction
+
+
+class Settings(ABC):
+    def __init__(self) -> None:
+        pygame.init()
+
+        device = models.Device()
+        resolution = pygame.display.Info()
+
+        self.start = True
+        self.clock = pygame.time.Clock()
+        self.keys = pygame.key.get_pressed()
+        self.event_type = None
+        self.event_key = None
+        self.display = resolution.current_w, resolution.current_h
+        self.screen = pygame.display.set_mode(self.display, pygame.RESIZABLE)
+        self.refresh_rate = device.get_refresh_rate()
+        self.current_direction = Direction.DOWN
+        self.delta_time = 0.0
+        self.speed = 225
+
+        self.direction_up_keys: List[bool] = []
+        self.direction_down_keys: List[bool] = []
+        self.direction_left_keys: List[bool] = []
+        self.direction_right_keys: List[bool] = []
+        self.all_direction_keys: List[bool] = []
+
+        self.player_pos = pygame.Vector2(
+            self.screen.get_width() / 2, self.screen.get_height() / 2)
+
+    def update(self):
+        """Update the game."""
+        self.keys = pygame.key.get_pressed()
+
+        for event in pygame.event.get():
+            self.event_type = event.type
+
+            if event.type == pygame.KEYUP:
+                self.event_key = event.key
+
+        # update the direction keys to keep the player moving in the same direction
+        self.direction_up_keys = [
+            self.keys[pygame.K_w], self.keys[pygame.K_UP]]
+        self.direction_down_keys = [
+            self.keys[pygame.K_s], self.keys[pygame.K_DOWN]]
+        self.direction_left_keys = [
+            self.keys[pygame.K_a], self.keys[pygame.K_LEFT]]
+        self.direction_right_keys = [
+            self.keys[pygame.K_d], self.keys[pygame.K_RIGHT]]
+        self.all_direction_keys = [*self.direction_up_keys, *self.direction_down_keys,
+                                   *self.direction_left_keys, *self.direction_right_keys]
+
+        pygame.display.update()
+        # update the delta time and clock tick rate to keep the game running at the same speed on all devices
+        self.delta_time = self.clock.tick(self.refresh_rate) / 1000
+
+    def handle_events(self):
+        """Handle the events."""
+        if self.event_type == pygame.QUIT:
+            self.exit()
+
+        if self.event_type == pygame.KEYUP and self.event_key == pygame.K_F11:
+            self.mouse_visible(True)
+            self.toggle_fullscreen()
+
+    def mouse_visible(self, visible: bool):
+        """Show or hide the mouse."""
+        pygame.mouse.set_visible(visible)
+
+    def load_music(self, music: str):
+        """Load the music."""
+        resources.music("village_vibe.mp3")
+
+    def toggle_fullscreen(self):
+        """Toggle fullscreen mode."""
+        pygame.display.toggle_fullscreen()
+
+    def exit(self):
+        """Exit the game."""
+        self.start = False
