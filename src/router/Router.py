f34d9eabc22ca898d82736871a151bc7b3686075
PerQuis! <thetahirpl@gmail.com>
2024-03-08 17:56:54 +0100

    Refactor Router class to use local method for getting assets path

--- src/router/Router.py
+++ src/router/Router.py
@@ -1,13 +1,15 @@
 import os
 from dataclasses import dataclass
 
-from .get_assets_path import get_assets_path
 
-# root levet directory for assets which are necessary.
-root: str = get_assets_path(os.getcwd())
-
-
-@dataclass
 class Router:
-    music: str = f"{root}/music"
-    sprites: str = f"{root}/packages/sprites_basic_pack/Characters"
+    def __init__(self):
+        self.music: str = f"{self.get_assets_path()}/music"
+        self.sprites: str = f"{self.get_assets_path()}/packages/sprites_basic_pack/Characters"
+
+    def get_assets_path(start_path):
+        for root, dirs, files in os.walk(os.getcwd()):
+            if "assets" in dirs:
+                return os.path.join(root, "assets")
+
+        return None
--- src/router/get_assets_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_assets_path(start_path):
-    for root, dirs, files in os.walk(start_path):
-        if "assets" in dirs:
-            return os.path.join(root, "assets")
-
-    return None
