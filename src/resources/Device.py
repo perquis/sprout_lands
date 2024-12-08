54d679e34c737d138ba89b7aa3338a61d721f553
Valentyn Zahrebelskyi <valentyn.zahrebelskyi@gmail.com>
2024-12-08 21:04:25 +0400

    Update Device.py

--- src/resources/Device.py
+++ src/resources/Device.py
@@ -1,12 +1,16 @@
 import os
-
 import pygame
-import win32api
+from typing import Tuple
 
+if os.name == "posix":
+    import Quartz
+elif os.mane == "nt":
+    import win32api
 
 class Device:
     def __init__(self) -> None:
-        self.__device = win32api.EnumDisplayDevices()
+        if os.name == "nt":
+            self.__device = win32api.EnumDisplayDevices()
 
     def get_refresh_rate(self) -> float:
         if os.name == "nt":
@@ -14,12 +18,24 @@ class Device:
                 self.__device.DeviceName, -1
             )
             refresh_rate = settings.DisplayFrequency
-            to_float_result = float(refresh_rate)
+            return float(refresh_rate)
+        elif os.name == "posix":
+            display = Quartz.CGMainDisplayID()
+            refresh_rate = Quartz.CGDisplayModeGetRefreshRate(
+                Quartz.CGDisplayCopyDisplayMode(display)
+            )
+            return refresh_rate if refresh_rate > 0 else 60.0  # Default to 60 Hz if unavailable
 
-            return to_float_result
+        return 60.0  # Default refresh rate
 
-        return 60.0
+    def get_display(self) -> Tuple[int, int]:
+        if os.name == "nt":
+            resolution = pygame.display.Info()
+            return resolution.current_w, resolution.current_h
+        elif os.name == "posix":
+            display = Quartz.CGMainDisplayID()
+            width = Quartz.CGDisplayPixelsWide(display)
+            height = Quartz.CGDisplayPixelsHigh(display)
+            return width, height
 
-    def get_display(self):
-        resolution = pygame.display.Info()
-        return resolution.current_w, resolution.current_h
+        return (0, 0)  # Default resolution if OS not supported
